72a39411220a6fd1b8b76e34a017d7ea738ca1a3
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2017-03-30 09:28:00 +1300

    Extract configuration to secrets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :redirect_if_not_google_authenticated
 end
--- config/application.rb
+++ config/application.rb
@@ -17,28 +17,17 @@ Bundler.require(*Rails.groups)
 
 module Invoicing
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.time_zone = "Wellington"
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
 
 SimpleGoogleAuth.configure do |config|
-  config.client_id = "the client ID as supplied by Google in step 2"
-  config.client_secret = "the client secret as supplied by Google in step 2"
-  config.redirect_uri = "http://localhost:8080/google-callback"
+  config.client_id = Rails.application.secrets.google_id
+  config.client_secret = Rails.application.secrets.google_secret
+  config.redirect_uri = Rails.application.secrets.google_redirect_url
+
   config.authenticate = lambda do |data|
-    data.email == "jakub.chodounsky@gmail.com"
+    data.email == Rails.application.secrets.authenticated_email
   end
 end
